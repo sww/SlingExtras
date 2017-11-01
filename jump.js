f9c960bfac7a20230c4f5c8e7f467b83230a25b8
Sean Wang <sean@decrypted.org>
2017-10-31 19:30:07 -0700

    Refactored everything into the SlingExtra object.

--- jump.js
+++ jump.js
@@ -1,44 +1,63 @@
-function $getChannelId(url) {
-    // https://watch.sling.com/watch?type=linear&id=22339910&channel=1081&action=resume
-    var re = /channel=(\d+)/;
-    var results = re.exec(url);
-    if (results !== null) {
-        return results[1];
+var SlingExtras = {
+    initialized: false,
+
+    init: function() {
+        this.angular = angular.element(document);
+        this.WatchService = this.angular.injector().get('WatchService');
+        this.AppConstants = this.angular.injector().get('AppConstants');
+
+        this.initialized = true;
+        console.log('initialized', this.initialized);
+    },
+
+    getChannelId: function(url) {
+        // https://watch.sling.com/watch?type=linear&id=22339910&channel=1081&action=resume
+        var re = /channel=(\d+)/;
+        var results = re.exec(url);
+        if (results !== null) {
+            return results[1];
+        }
+
+        return null;
+    },
+
+    switchToChannel: function(channelId) {
+        console.log("Switching to channel", channelId);
+        var channelId = parseInt(channelId, 10);
+        var channel = {
+            "id": channelId,
+            "type": "channel"
+        }
+
+        this.WatchService.watch(channel, this.AppConstants.VIDEO.ACTIONS.RESUME);
+    },
+
+    jump: function() {
+        var currentChannelId = this.getChannelId(window.location.href);
+        var previousChannelId = localStorage['lastChannelId'];
+
+        if (!previousChannelId) {
+            console.log("No channel to jump back to.");
+            return;
+        }
+
+        if (currentChannelId) {
+            localStorage['lastChannelId'] = currentChannelId;
+        }
+
+        this.switchToChannel(previousChannelId);
     }
-
-    return null;
-}
-
-function $switchToChannel(channelId) {
-    console.log("Switching to channel", channelId);
-    var WatchService = angular.element(angular.element(document)[0]).injector().get('WatchService');
-    var AppConstants = angular.element(angular.element(document)[0]).injector().get('AppConstants');
-
-    var channelId = parseInt(channelId, 10);
-    var channel = {
-        "id": channelId,
-        "type": "channel"
-    }
-
-    WatchService.watch(channel, AppConstants.VIDEO.ACTIONS.RESUME);
 }
 
 window.addEventListener("keyup", function(e) {
-    if (e.key && e.key.toLowerCase() !== "j") {
-        return;
+    switch (e.key.toLower()) {
+    case 'i':
+        if (!SlingExtras.initialized) {
+            SlingExtras.init();
+        }
+        break;
+    case 'j':
+        SlingExtras.jump();
+        break;
     }
-
-    var currentChannelId = $getChannelId(window.location.href);
-    var previousChannelId = localStorage['lastChannelId'];
-
-    if (!previousChannelId) {
-        console.log("No channel to jump back to.");
-        return;
-    }
-
-    if (currentChannelId) {
-        localStorage['lastChannelId'] = currentChannelId;
-    }
-
-    $switchToChannel(previousChannelId);
-});
+}, false);
